790e6c70c3c03ea8e3278fa2f4c6dc5a94e00a48
JordAnne <jbethune37@gmail.com>
2023-04-02 15:39:53 -0400

    added code to create departments, roles, and employess

--- index.js
+++ index.js
@@ -1,42 +1,78 @@
 const express = require("express");
-const mysql = require("mysql");
 
-const db = mysql.createConnections({
-    host: "localhost",
-    user: "root",
-    password: "funfun",
-    database: "nodemysql",
-});
+const mysql = require("mysql2");
 
-db.connect((err)=>{
-    if(err) {
-        throw err;
-    }
-
-    console.log("Mysqlconnect");
-});
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get("/creatdb", (req,res) =>{
-    let sql = "Create database";
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-    db.query(sql,(err) =>{
-        if(err){
-            throw err;
-        }
-        res.send("Database created");
-    });
-});
+// connectin database
+const db = mysql.createConnections(
+    {
+    host: "localhost",
+    user: "root",
+    password: "Jimmy2021!",
+    database: "trackerdb",
+},
+    console.log("connected to the database")
+);
 
-app.get("/createemployee", (req,res) =>{
+
+
+// app.get("/creatdb", (req,res) =>{
+//     let sql = "Create database";
+
+//     db.query(sql,(err) =>{
+//         if(err){
+//             throw err;
+//         }
+//         res.send("Database created");
+//     });
+// });
+
+
+// created department
+app.post("/createdepartment", (req,res) =>{
     let sql =
-    "CREATE TABLE employee(id INT PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(30), role_id INT, manager_id INT)";
+    "CREATE TABLE department(id INT PRIMARY KEY, name VARCHAR(30))";
 
     db.query(sql,(err) =>{
         if(err){
             throw err;
         }
-        res.send("Employee table")
+        res.send("department table made")
     });
 });
+
+
+// create role
+app.post("/createrole", (req,res) =>{
+    let sql =
+    "CREATE TABLE role(id INT PRIMARY KEY, title VARCHAR(30), salary DECIMAL, department_id INT)";
+
+    db.query(sql,(err) =>{
+        if(err){
+            throw err;
+        }
+        res.send("role table made")
+    });
+});
+
+
+
+// created employee
+app.post("/createemployee", (req,res) =>{
+    const sql = "CREATE TABLE employee(id INT PRIMARY KEY, first_name VARCHAR(30), last_name VARCHAR(30), role_id INT, manager_id INT)";
+
+    db.query(sql,(err) =>{
+        if(err){
+            throw err;
+        }
+        res.send("Employee table made")
+    });
+});
+
+
+app.get("")
